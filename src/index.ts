3e37e5a43b35a70c0f4c0b2a2fd3269753b272eb
Shreyas Tallamraju <shreyastallamraju@gmail.com>
2021-11-03 13:16:30 -0500

    initial changes to simpleboolean lang package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@codemirror/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const simplebooleanLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
@@ -25,6 +25,6 @@ export const EXAMPLELanguage = LRLanguage.define({
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function simpleboolean() {
+  return new LanguageSupport(simplebooleanLanguage)
 }
